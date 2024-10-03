aab81f2e88c2d4a11984c0e0a0b8217d42959682
Jamcda <jamcdaniels99@gmail.com>
2024-10-03 17:32:55 -0400

    Security, Post Drafts, Domain, Comment model, and Database connected to post model created

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'postgres',
+        'PASSWORD': 'jacob',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
